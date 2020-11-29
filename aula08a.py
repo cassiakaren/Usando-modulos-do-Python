bbca6d94e1623e005e5faec0d38d7af66264e356
Cassia Karen de Sousa Santos <cassiakaren56@gmail.com>
2020-11-29 13:51:20 -0300

    aula08
    importei aa bibliotecas  emoji, random, math

--- aula08a.py
+++ aula08a.py
@@ -1,4 +1,10 @@
-import math
+'''from math import sqrt, ceil
 num = int(input('digite um numero: '))
-raiz= math.sqrt(num)
-print('a raiz de {} é igual a {}'.format(num,math.floor(raiz)))
+raiz = sqrt(num)
+print('a raiz de {} é igual a {:.2f} '.format(num,ceil (raiz)))'''
+'''import random #gera um numero aleatorio de 0 a 1
+num = random.randint(1, 100) #randint gera numeros inteiros infinitos
+print(num)'''
+
+import emoji
+print (emoji.emojize('Olá, mundo :earth_americas:',use_aliases=True))
